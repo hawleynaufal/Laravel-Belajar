f38105454486cafed7df2f343fbfb245dcd2884b
gempurbayu <aji123zz@gmail.com>
2016-12-22 16:11:44 +0700

    coba merge

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Tambahan
                 </div>
 
                 <div class="links">
